815ed1b5720b20a3a49fc4c70f9d2906a43ccb87
Luis Machuca Álvarez <luis.machuca.alvarez@outlook.com>
2019-05-20 07:34:08 -0400

    pruebas con funciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,44 @@
 //Se declaran valiables de los elementos de la página
-const tarea = document.getElementById('tareas-ingresadas');
+const tareasIngresadas = document.getElementById('tareas-ingresadas');
+const formularioTareas = document.getElementById('formulario');
+
+//Event listeners
+
+eventListeners();
+
+function eventListeners() {
+    formularioTareas.addEventListener('submit', ingresarTarea);
+}
 
 
-console.log(tarea);
+
+//Funciones
+
+
+
+//Funcion ingresar tarea
+function ingresarTarea(e) {
+    e.preventDefault();
+    //Se extrae valor de elemento tarea
+    const descripcionTarea = document.getElementById('descripcion-tarea').value;
+
+    //Crear boton eliminar
+    const botonEliminar = document.createElement('a');
+    botonEliminar.classList = 'delete';
+
+
+    //Crear elemento y añadirlo a la lista de tareas
+    const li = document.createElement('li');
+    li.innerText = descripcionTarea;
+
+    //añade el botón de borrar a la tarea
+    li.appendChild(botonEliminar);
+
+    //Añade la tarea a la lista
+    tareasIngresadas.appendChild(li);
+}
+
+//Pasos para crear elementos con JavaScript
+// 1.- Declarar variables
+// 2.- Crear los event listeners
+// 3.- Crear las funciones
