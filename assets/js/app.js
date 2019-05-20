5948de7a09a3e2564a849773717a323667623645
tecsya02 <tecsya02@sbif.cl>
2019-05-20 18:06:34 -0400

    Funciones de guardar y eliminar creados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 //Se declaran valiables de los elementos de la página
 const tareasIngresadas = document.getElementById('tareas-ingresadas');
+
 const formularioTareas = document.getElementById('formulario');
 
 //Event listeners
@@ -8,6 +9,9 @@ eventListeners();
 
 function eventListeners() {
     formularioTareas.addEventListener('submit', ingresarTarea);
+
+    //Eliminar tarea ingresada
+    tareasIngresadas.addEventListener('click', borrarTarea);
 }
 
 
@@ -21,21 +25,32 @@ function ingresarTarea(e) {
     e.preventDefault();
     //Se extrae valor de elemento tarea
     const descripcionTarea = document.getElementById('descripcion-tarea').value;
+    const estimacionTarea = document.getElementById('estimacion-tarea').value;
 
     //Crear boton eliminar
     const botonEliminar = document.createElement('a');
-    botonEliminar.classList = 'delete';
+    botonEliminar.classList.add('delete', 'is-small', 'borrar-tarea');
 
 
     //Crear elemento y añadirlo a la lista de tareas
     const li = document.createElement('li');
-    li.innerText = descripcionTarea;
+    li.innerText = descripcionTarea + ", Estimación: " + estimacionTarea;
 
     //añade el botón de borrar a la tarea
     li.appendChild(botonEliminar);
 
     //Añade la tarea a la lista
     tareasIngresadas.appendChild(li);
+}
+
+
+function borrarTarea(e){
+    e.preventDefault();
+    if(e.target.className === 'delete is-small borrar-tarea'){
+        // console.log('Funciona');
+        console.log(e.target.parentElement.remove());
+        alert('Tarea eliminada');
+    }
 }
 
 //Pasos para crear elementos con JavaScript
